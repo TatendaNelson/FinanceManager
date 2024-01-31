5ab3e546a4386890c6d7bbd06e10b39788856ac6
Sharonrose29 <152410233+Sharonrose29@users.noreply.github.com>
2024-01-31 02:22:35 +0200

    Update CategoriesUi.Designer.cs
    Implemented 'Add Category' functionality in CategoriesUi, allowing users to easily create new budget categories. Enhanced the UI with a smooth interface, input validation, and an intuitive 'ADD' button for a seamless user experience.

--- Finance Manager/CategoriesUi.Designer.cs
+++ Finance Manager/CategoriesUi.Designer.cs
@@ -1,1 +1,343 @@
-﻿
+﻿namespace Finance_Manager
+{
+    partial class CategoriesUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuSettings = new System.Windows.Forms.Label();
+            this.menuIncome = new System.Windows.Forms.Label();
+            this.menuBills = new System.Windows.Forms.Label();
+            this.menuCategories = new System.Windows.Forms.Label();
+            this.menuNotifications = new System.Windows.Forms.Label();
+            this.menuBudget = new System.Windows.Forms.Label();
+            this.menuHome = new System.Windows.Forms.Label();
+            this.menuUsername = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(-9, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1355, 615);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.AutoScroll = true;
+            this.panel4.AutoSize = true;
+            this.panel4.Controls.Add(this.flowLayoutPanel1);
+            this.panel4.Controls.Add(this.label12);
+            this.panel4.Controls.Add(this.panel6);
+            this.panel4.Location = new System.Drawing.Point(261, 69);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1023, 540);
+            this.panel4.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(134, 257);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(531, 256);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(130, 234);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(89, 21);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Categories";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.Controls.Add(this.button2);
+            this.panel6.Controls.Add(this.label14);
+            this.panel6.Controls.Add(this.textBox1);
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Location = new System.Drawing.Point(134, 19);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(523, 169);
+            this.panel6.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(158)))), ((int)(((byte)(255)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(158)))), ((int)(((byte)(255)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.button2.Location = new System.Drawing.Point(294, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 25);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "ADD";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Yu Gothic UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(35, 73);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 17);
+            this.label14.TabIndex = 2;
+            this.label14.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(35)))), ((int)(((byte)(126)))));
+            this.panel7.Controls.Add(this.label13);
+            this.panel7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(523, 44);
+            this.panel7.TabIndex = 0;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(5, 11);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(112, 21);
+            this.label13.TabIndex = 1;
+            this.label13.Text = "Add Category";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(35)))), ((int)(((byte)(126)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.menuSettings);
+            this.panel2.Controls.Add(this.menuIncome);
+            this.panel2.Controls.Add(this.menuBills);
+            this.panel2.Controls.Add(this.menuCategories);
+            this.panel2.Controls.Add(this.menuNotifications);
+            this.panel2.Controls.Add(this.menuBudget);
+            this.panel2.Controls.Add(this.menuHome);
+            this.panel2.Controls.Add(this.menuUsername);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(224, 615);
+            this.panel2.TabIndex = 0;
+            // 
+            // menuSettings
+            // 
+            this.menuSettings.AutoSize = true;
+            this.menuSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuSettings.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuSettings.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.menuSettings.Location = new System.Drawing.Point(53, 456);
+            this.menuSettings.Name = "menuSettings";
+            this.menuSettings.Size = new System.Drawing.Size(63, 20);
+            this.menuSettings.TabIndex = 8;
+            this.menuSettings.Text = "Settings";
+            this.menuSettings.Click += new System.EventHandler(this.menuSettings_Click);
+            // 
+            // menuIncome
+            // 
+            this.menuIncome.AutoSize = true;
+            this.menuIncome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuIncome.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuIncome.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.menuIncome.Location = new System.Drawing.Point(53, 219);
+            this.menuIncome.Name = "menuIncome";
+            this.menuIncome.Size = new System.Drawing.Size(59, 20);
+            this.menuIncome.TabIndex = 7;
+            this.menuIncome.Text = "Income";
+            this.menuIncome.Click += new System.EventHandler(this.menuIncome_Click);
+            // 
+            // menuBills
+            // 
+            this.menuBills.AutoSize = true;
+            this.menuBills.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuBills.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuBills.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.menuBills.Location = new System.Drawing.Point(53, 261);
+            this.menuBills.Name = "menuBills";
+            this.menuBills.Size = new System.Drawing.Size(36, 20);
+            this.menuBills.TabIndex = 6;
+            this.menuBills.Text = "Bills";
+            this.menuBills.Click += new System.EventHandler(this.menuBills_Click);
+            // 
+            // menuCategories
+            // 
+            this.menuCategories.AutoSize = true;
+            this.menuCategories.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuCategories.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuCategories.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.menuCategories.Location = new System.Drawing.Point(53, 303);
+            this.menuCategories.Name = "menuCategories";
+            this.menuCategories.Size = new System.Drawing.Size(81, 20);
+            this.menuCategories.TabIndex = 5;
+            this.menuCategories.Text = "Categories";
+            // 
+            // menuNotifications
+            // 
+            this.menuNotifications.AutoSize = true;
+            this.menuNotifications.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuNotifications.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuNotifications.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.menuNotifications.Location = new System.Drawing.Point(53, 387);
+            this.menuNotifications.Name = "menuNotifications";
+            this.menuNotifications.Size = new System.Drawing.Size(97, 20);
+            this.menuNotifications.TabIndex = 4;
+            this.menuNotifications.Text = "Notifications";
+            this.menuNotifications.Click += new System.EventHandler(this.menuNotifications_Click);
+            // 
+            // menuBudget
+            // 
+            this.menuBudget.AutoSize = true;
+            this.menuBudget.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuBudget.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuBudget.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.menuBudget.Location = new System.Drawing.Point(53, 345);
+            this.menuBudget.Name = "menuBudget";
+            this.menuBudget.Size = new System.Drawing.Size(58, 20);
+            this.menuBudget.TabIndex = 3;
+            this.menuBudget.Text = "Budget";
+            this.menuBudget.Click += new System.EventHandler(this.menuBudget_Click);
+            // 
+            // menuHome
+            // 
+            this.menuHome.AutoSize = true;
+            this.menuHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuHome.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuHome.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.menuHome.Location = new System.Drawing.Point(53, 177);
+            this.menuHome.Name = "menuHome";
+            this.menuHome.Size = new System.Drawing.Size(50, 20);
+            this.menuHome.TabIndex = 2;
+            this.menuHome.Text = "Home";
+            this.menuHome.Click += new System.EventHandler(this.menuHome_Click);
+            // 
+            // menuUsername
+            // 
+            this.menuUsername.AutoSize = true;
+            this.menuUsername.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuUsername.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.menuUsername.Location = new System.Drawing.Point(53, 120);
+            this.menuUsername.Name = "menuUsername";
+            this.menuUsername.Size = new System.Drawing.Size(81, 21);
+            this.menuUsername.TabIndex = 1;
+            this.menuUsername.Text = "username";
+            this.menuUsername.Click += new System.EventHandler(this.menuUsername_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(170)))));
+            this.label2.Location = new System.Drawing.Point(39, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 106);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "FM";
+            // 
+            // CategoriesUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 670);
+            this.Controls.Add(this.panel1);
+            this.MinimizeBox = false;
+            this.Name = "CategoriesUi";
+            this.Text = "CategoriesUi";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label menuSettings;
+        private System.Windows.Forms.Label menuIncome;
+        private System.Windows.Forms.Label menuBills;
+        private System.Windows.Forms.Label menuCategories;
+        private System.Windows.Forms.Label menuNotifications;
+        private System.Windows.Forms.Label menuBudget;
+        private System.Windows.Forms.Label menuHome;
+        private System.Windows.Forms.Label menuUsername;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
